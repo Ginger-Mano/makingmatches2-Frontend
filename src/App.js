84c599dfa5fa80c4b3be48f8a30ddafdad04375a
Manon Sainton <manonsainton.dev@gmail.com>
2021-09-20 11:34:52 -0400

    start fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
+import { data } from 'browserslist';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     <h2>Test</h2>
-    </div>
-  );
-}
+
+class App extends Component {
+
+  state = {
+    dataArr: []
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/cards`)
+    .then(res => res.json())
+    .then(dataArr => console.log(dataArr))
+  }
+
+  render () {
+    return (
+      <div className="App">
+  
+       <h2>Test</h2>
+
+      </div>
+    );
+  }
+  }
 
 export default App;
